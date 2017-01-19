50eae8843198255ae0e4bea24477efb9d88aa155
earlgreymacaron <yeaseul.parky@gmail.com>
2017-01-20 02:53:46 +0900

    ys

---
+++ app/src/main/java/com/cs496/gogofishy/App.java
@@ -0,0 +1,20 @@
+package com.cs496.gogofishy;
+
+import android.app.Application;
+
+import java.io.IOException;
+
+import pl.droidsonroids.gif.GifDrawable;
+import pl.droidsonroids.gif.GifImageView;
+
+/**
+ * Created by q on 2017-01-19.
+ */
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
--- app/src/main/java/com/cs496/gogofishy/MainActivity.java
+++ app/src/main/java/com/cs496/gogofishy/MainActivity.java
@@ -1,7 +1,15 @@
 package com.cs496.gogofishy;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import java.io.IOException;
+
+import pl.droidsonroids.gif.GifDrawable;
+import pl.droidsonroids.gif.GifImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RelativeLayout bg = (RelativeLayout) findViewById(R.id.activity_main);
+        GifDrawable oceangif = null;
+        try {
+            oceangif = new GifDrawable(getAssets(), "beach.gif" );
+            bg.setBackground(oceangif);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
